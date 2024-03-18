12380c34e404f048312ee1aa77a5e488c9227acb
Mikhail Nepochatov <m380680908367@gmail.com>
2024-03-18 17:37:23 +0200

    28-reducer-function

---
+++ 28-reducer/reducer.js
@@ -0,0 +1,41 @@
+const initialState = []
+
+function reducer (state, action) {
+    switch (action.type) {
+        case "ADD_NAME":
+            return [...state, action.payload]
+        case "DELETE_NAME": 
+            return state.filter(userName => userName !== action.payload)
+        case "CLEAR_NAMES":
+            return []
+        default:
+            return state
+    }
+
+
+    // if (action.type === "ADD_NAME") {
+    //     return [...state, action.payload]
+    // }
+    // if (action.type === "DELETE_NAME") {
+    //     return state.filter(userName => userName !== action.payload)
+    // }
+    // if (action.type === "CLEAR_NAMES") {
+    //     return []
+    // }
+    // return state
+}
+
+let newState = reducer(initialState, {type: "ADD_NAME", payload: "Misha"})
+console.log(newState)
+
+newState = reducer(newState, {type: "ADD_NAME", payload: "Boris"})
+console.log(newState)
+
+newState = reducer(newState, {type: "DELETE_NAME", payload: "Boris"})
+console.log(newState)
+
+newState = reducer(newState, {type: "CLEAR_NAMES"})
+console.log(newState)
+
+newState = reducer(newState, {type: "ADD_NAME", payload: "Misha"})
+console.log(newState)
